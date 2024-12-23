16f92e70a213b6685199fc5ae2484a7df9214f16
abdullahalkawser <abdullahalkawser1@gmail.com>
2024-12-23 22:55:05 +0600

    data types

--- Variable DataTypesOperators/demo.cpp
+++ Variable DataTypesOperators/demo.cpp
@@ -1,12 +1,62 @@
-#include <iostream> // preprosessor directive 
+// #include <iostream> // preprosessor directive 
+// using namespace std;
+
+
+
+// int main (){
+//     cout << "hello world \n abdullah al kawsers"<< endl;// <<"/n" new line faster<< endl
+//     cout << "abdullah al " <<"kawser";// terminator;
+    
+//  return 0;
+// }
+
+
+//Variables
+
+#include <iostream> 
 using namespace std;
 
-int main (){
-    cout << "hello world \n abdullah al kawsers"<< endl;// <<"/n" new line faster<< endl
-    cout << "abdullah al " <<"kawser";// terminator;
+// primitive data types 
+
+int main() {
+    string fullname = "Abdullah Al Kawser";
+    int age = 23;
+    char grade = 'A';
+    float Pi= 2.898f;
+    bool issave = true; // ture mean 1 false means 0
+        bool issaves = false; // ture mean 1 false means 0
+
+
+    // cout << sizeof(fullname) << " " <<sizeof (age); // sizeof koto bits or bytes
     
- return 0;
+    // cout << (grade) ;
+//  cout << sizeof(Pi);
+  cout << issave << endl << issaves;
+    return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //prctice
