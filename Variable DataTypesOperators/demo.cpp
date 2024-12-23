9a7722bcdd9f127b4989f20bc752f6dd1b2f2f9c
abdullahalkawser <abdullahalkawser1@gmail.com>
2024-12-23 22:26:40 +0600

    Output in C++ How does code run?  Boilerplate  Comments

--- Variable DataTypesOperators/demo.cpp
+++ Variable DataTypesOperators/demo.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <iostream> // preprosessor directive 
+using namespace std;
 
-int main() {
-    std::cout << "hello";
-    return 0;
+int main (){
+    cout << "hello world \n abdullah al kawsers"<< endl;// <<"/n" new line faster<< endl
+    cout << "abdullah al " <<"kawser";// terminator;
+    
+ return 0;
 }
+
+
+//prctice
+
+// #include <iostream>
+// using namespace std;
+
+// int main (){
+//     cout << "Anowaare hossain \n babu";//
+// }
+
+
+
+
+
