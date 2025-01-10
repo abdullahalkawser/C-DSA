30fdb6f39914b351c985bcd33e5ffd5c730d2f58
alkawseraak <kawserkawser287@gmail.com>
2025-01-11 05:09:43 +0600

    factorial

--- Conditional Statements & Loops/loop.cpp
+++ Conditional Statements & Loops/loop.cpp
@@ -59,3 +59,84 @@ for (int i = 0; i < 10; i++) {
   }
   cout << i << "\n";
 }
+
+// sum  odd and even number 
+
+
+
+int main() {
+    int n = 10;
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 2 != 0) {  // Check if the number is odd if i % 2 ==0  even number
+            sum += i;  // Add the odd number to the sum
+        }
+    }
+
+    // Display the total sum of odd numbers
+    cout << "Total sum of odd numbers: " << sum << "\n";
+
+    return 0;
+}
+
+//prime number 
+
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n = 20;
+    cout << "Prime: ";
+    for (int i = 1; i <= n; i++) if (isPrime(i)) cout << i << " ";
+
+    cout << "\nNon-prime: ";
+    for (int i = 1; i <= n; i++) if (!isPrime(i)) cout << i << " ";
+    
+    return 0;
+}
+
+
+///nested loop 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;  // Number of rows for the table
+    int l = 10; // Number of columns for the table (fixed the missing semicolon)
+    
+    for (int i = 1; i <= n; i++) {  // Outer loop for rows
+        for (int j = 1; j <= l; j++) {  // Inner loop for columns
+            cout << i * j << "\t";  // Print multiplication of i and j
+        }
+        cout << endl;  // Move to the next line after each row
+    }
+    return 0;
+}
+
+// factorial 
+
+
+
+int main() {
+    int n;
+    cin >> n;
+
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+
+    cout << result << endl;
+    return 0;
+}
+
+
