ce5bad7b3df1a3206c1cef09d8dbab44bfa471ac
alkawseraak <kawserkawser287@gmail.com>
2025-01-11 05:26:59 +0600

    print a triangle pattern in C++

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -1,1 +1,17 @@
-hiiiiii
+#include <iostream>
+using namespace std;
+
+void printTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n = 5; // Example size of the triangle
+    printTriangle(n);
+    return 0;
+}
