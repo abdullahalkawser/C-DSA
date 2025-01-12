9582dbae7da2958d0ebaa107d8c6ef706f31cec4
alkawseraak <kawserkawser287@gmail.com>
2025-01-12 16:28:11 +0600

    Inverted Right-Angled Triangle

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -33,3 +33,23 @@ int main() {
 
     return 0;
 }
+
+//Inverted Right-Angled Triangle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+
+    for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
