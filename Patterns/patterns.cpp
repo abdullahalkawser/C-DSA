620989d7eb02bc3f5fb97adcf944c429ec7cfca8
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-01-16 12:40:39 +0600

    pattern Checkerboard Pattern

--- Patterns/patterns.cpp
+++ Patterns/patterns.cpp
@@ -112,3 +112,28 @@ int main() {
 
     return 0;
 }
+
+// Checkerboard Pattern
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the board: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if ((i + j) % 2 == 0) {
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
