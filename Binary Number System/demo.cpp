9a4e424c8bb39f69c8d86f7b5aeb57f5412f33cf
abdullahalkawser <abdullahalkawser1@gmail.com>
2025-01-19 04:27:01 +0600

    loop to convert numbers 1 to 10 from decimal to binary

--- Binary Number System/demo.cpp
+++ Binary Number System/demo.cpp
@@ -20,3 +20,26 @@ int main() {
     cout << "Binary: " << binary << endl;
     return 0;
 }
+
+
+// convert numbers from 1 to 10 into binary,
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int n = 1; n <= 10; n++) { // Loop from 1 to 10
+        int num = n;
+        string binary = "";
+
+        // Convert current number to binary
+        do {
+            binary = to_string(num % 2) + binary;
+            num /= 2;
+        } while (num > 0);
+
+        // Output the binary representation
+        cout << "Decimal: " << n << " => Binary: " << binary << endl;
+    }
+    return 0;
+}
